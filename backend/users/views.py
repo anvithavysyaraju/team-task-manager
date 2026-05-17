5c7add39292dde81090b6aff751c9a2df3ed9263
Vysyaraju Anvitha <anvitha@gmail.com>
2026-05-17 18:10:22 +0530

    Fix registration authentication

--- backend/users/views.py
+++ backend/users/views.py
@@ -19,7 +19,8 @@ User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = [AllowAny]
+    authentication_classes = []
 
 
 class MeView(APIView):
